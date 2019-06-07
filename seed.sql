9b97ae4ba736e6863560dde27e8f5c0f9572278f
RedSW20 <kevbailey1227@gmail.com>
2019-06-07 13:18:59 -0500

    set up sql seed

---
+++ seed.sql
@@ -0,0 +1,18 @@
+INSERT INTO products(product_name,department_name,price,stock_quantity)
+VALUES ('Puma Suede Shoes','Sports',40.00,10),
+	('Galaxy s10+ Case','Accessories',50.99,100),
+    ('Razor Mouse','Electronics',20.50,5),
+    ('Bose Speakers','Electronics',100.00,1),
+    ('California King Mattress set','Furniture',20.00,10),
+    ('Digital Storm Gaming PC','Electronics',1769.99,5),
+    ('Asus Monitor','Electronics',89.99,5),
+    ('Lord of the Rings: The Fellowship of the Ring','Books',10.00,10),
+    ('Lord of the Rings: The Two Towers','Books',10.00,10),
+    ('Lord of the Rings: The Return of the King','Books',10.00,10)
+;
+
+INSERT INTO departments(department_name,over_head_costs)
+VALUES ('Sports',10000.00),
+		('Electronics',20000.00),
+        ('Accessories',30000.00),
+        ('Books',40000.00)

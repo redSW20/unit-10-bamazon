0944de7a104cef3a1f9fb079028c7e680df2e638
RedSW20 <kevbailey1227@gmail.com>
2019-06-07 13:22:16 -0500

    finished customer js file

--- customer.js
+++ customer.js
@@ -46,3 +46,67 @@ function selectItem() {
         });
 };
 
+// Prompts the user for the quantity of the item they'd like to purchase
+function selectQuantity(id){
+    inquirer
+        .prompt([
+            {
+                name: "quantity",
+                message: "Please enter the quantity you'd like to purchase: ",
+                type: "input",
+                validate: function validateNumber(quantity){
+                    if(!parseInt(quantity)) return false;
+                    return true;
+                }
+            }
+        ])
+        .then(answers => {
+            validateOrder(id,parseInt(answers.quantity));
+        });
+};
+
+function validateOrder(id,quantity){
+    connection.query({
+        sql: 'SELECT * FROM products WHERE stock_quantity >= ? AND item_id=?',
+        values: [quantity,id]
+    },
+        function (error, results) {
+            if (error) throw error;
+            if(!results.length){
+                console.log("Insufficient Quantity");
+                connection.end();
+                return;
+            }
+            var totalPrice = toFixedTrunc((parseFloat(results[0].price)*quantity),2);
+            var newQuantity = results[0].stock_quantity-quantity;
+            var newSales = parseFloat(results[0].product_sales)+parseFloat(totalPrice);
+            console.log("Your order costs a total of: $" + totalPrice);
+            updateDb(newQuantity,id,newSales);
+        });
+};
+
+function updateDb(newQuantity,id,newSales){
+    connection.query({
+        sql: 'UPDATE products SET stock_quantity=?,product_sales=? WHERE item_id=?',
+        values: [newQuantity, newSales,id]
+    },
+        function (error, results) {
+            if (error) throw error;
+            connection.end();
+        });
+};
+
+// Borrowed from Stackoverflow, lots of answers but this one seemed most accurate to solve issue I was having.
+
+// https://stackoverflow.com/questions/4187146/truncate-number-to-two-decimal-places-without-rounding/11818658
+
+function toFixedTrunc(value, n) {
+    const v = value.toString().split('.');
+    if (n <= 0) return v[0];
+    let f = v[1] || '';
+    if (f.length > n) return `${v[0]}.${f.substr(0,n)}`;
+    while (f.length < n) f += '0';
+    return `${v[0]}.${f}`
+};
+
+init();
